d1cb431a47794b2b4ab00d28545c2c32501144e3
ksksksks-dev <sharma.kaustubh99@gmail.com>
2021-10-02 12:58:53 +0530

    Create deploy.py
    Create python script to deploy SimpleStorage

---
+++ deploy.py
@@ -0,0 +1,29 @@
+import json
+import solcx
+from solcx import compile_standard
+
+# solcx.install_solc()
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+)
+
+with open("./compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
